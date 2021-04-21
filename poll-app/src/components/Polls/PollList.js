6dbf50b32671dd430925b52619cd94ffe521b232
White Joan <joannnabiala@gmail.com>
2021-04-21 12:15:00 +0200

    Frontend: List of polls created by current user added, redirect to detail of poll added.

--- poll-app/src/components/Polls/PollList.js
+++ poll-app/src/components/Polls/PollList.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import {Link} from "react-router-dom";
 import {connect} from 'react-redux'
 import {getPolls} from "../../actions/pollsActions";
 
@@ -15,36 +16,25 @@ class pollList extends Component {
 
     return (
       <div>
+        <h3>Moje ankiety</h3>
         {polls.map(item => {
+          const linkTo = "/polls/show/" + item.id;
           return (
             <React.Fragment key={item.id}>
               <div>
-                <p>Nazwa ankiety: {item.poll_name}</p>
-                <p>Opis ankiety: {item.poll_description}</p>
-                <ul>
-                  {item.questions.map((question) => {
-                    return (
-                      <div>
-                        <li>
-                          Treść pytania: {question.question_description}
-                        </li>
-                        <ul>
-                          {question.answers.map((answer) => {
-                            return (
-                              <div>
-                                Odpowiedź: {answer.answer_description}
-                              </div>
-                            )
-                          })
-                          }
-                        </ul>
-                      </div>
-                    )
-                  })
-                  }
-                </ul>
+                <div className="ui relaxed divided list">
+                  <div className="item">
+                    <i className="large edit outline middle aligned icon"/>
+                    <div className="content">
+                      <Link to={linkTo}>
+                        <a className="header">Nazwa ankiety: {item.poll_name}</a>
+                      </Link>
+                      <div className="description">Opis ankiety: {item.poll_description}</div>
+                    </div>
+                  </div>
+                </div>
+                <hl/>
               </div>
-              <p>________________</p>
             </React.Fragment>
           )
         }
